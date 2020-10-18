1e9f901131182017a5578dd183c58428fc2020c5
alexfoz <alexandrehollanda@gmail.com>
2020-10-18 18:34:32 -0300

    revisão férias

---
+++ ferias/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from usuario.models import Usuario
+
+
+
+class Ferias(models.Model):
+    inicio = models.DateField(max_length=20)
+    fim = models.DateField(max_length=20)
+    descricao = models.CharField(max_length=240)
+    justificativa = models.CharField(max_length=480)
+    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    aprovador = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.solicitante
